035a5de04a390ec83b6d08a710da15953d958a92
Robert Gulian <bgulian@gmail.com>
2024-01-21 20:18:29 -0500

    violet menu items

--- src/components/landing_page.js
+++ src/components/landing_page.js
@@ -36,7 +36,7 @@ const LandingPage = (props) => {
       mitemBlock: {
         padding: "10px 10px 10px 10px",
         margin: "0px 70px 0px 100px",
-        color: 'blue'
+        color: 'violet'
       }
   };
   return (
