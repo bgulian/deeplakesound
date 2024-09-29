cca9931a4c0ada47c46650911dc5a1feccec5e90
Robert Gulian <bgulian@gmail.com>
2024-09-28 22:17:21 -0400

    new gear

--- src/components/just_the_right_gear.js
+++ src/components/just_the_right_gear.js
@@ -74,14 +74,14 @@ const JustTheRightGear = () => {
 				<li>RND 511 (2)</li>
 				<li>RND 517 </li>
 				<li>UA 6176 </li>
+				<li>UA LA-610 </li>
 				<li>Vintech x73i (2)</li>
 				<li>Grace Design m201 mk2 (2 channels)</li>
 				<li>DAK II mk2 (2 channels)</li>
-				<li>Dbx 760 x (2 channels)</li>
 				<li>API 512c</li>
 				<li>CAPI 312 (2)</li>
 				<li>Silver Bullet (2 channels)</li>
-				<li>SSL SiX</li>
+				<li>SSL SiX (2 channels)</li>
 				<li>Audio Design Pacifica (2 channels)</li>
 				<li>UAD (x8) Unison Pres (4 channels)</li>
 			</ul>
@@ -104,9 +104,11 @@ const JustTheRightGear = () => {
 				<li>Shadow Hills Vandergraph (2 channels)</li>
 				<li>Elysia XFilter (stereo)</li>
 				<li>Audioscape EQP-A (2)</li>
+				<li>Audioscape Opto Comp</li>
+				<li>Audioscape 76D Comp (2)</li>
 				<li>Harrison Comp (2)</li>
 				<li>Great River Harrison 32 EQ</li>
-				<li>SSL E-EQ</li>
+				<li>SSL 611-EQ (2)</li>
 				<li>UA 6176 Compressor></li>
 				<li>UA LA-610 Compressor></li>
 				<li>SSL SiX - SSL Buss Comp (2)</li>
